ca40adda00b8480c76d7e5fd482333b046556fd7
E-E-Ewoma-Zino <eewoma75@gmail.com>
2022-12-29 09:36:29 +0100

    cleaned the about page

--- assets/js/includes.js
+++ assets/js/includes.js
@@ -2,6 +2,7 @@
 
 $("head").load("/partials/_head.html");
 $("header").load("/partials/_nav.html");
+$("#testimonials").load("/components/testimonials.html");
 $("#contactForm").load("/components/contactForm.html");
 $("footer").load("/partials/_footer.html", ()=>{
 	$("#include").prepend(`<script src="/assets/js/bootstrap.min.js"></script>`);
