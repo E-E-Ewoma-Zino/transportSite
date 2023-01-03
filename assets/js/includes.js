33b61306598666b9a609cfa573d9e000b46d6849
E-E-Ewoma-Zino <eewoma75@gmail.com>
2023-01-03 16:11:04 +0100

    Completed Contact form and added contact address and map

--- assets/js/includes.js
+++ assets/js/includes.js
@@ -4,6 +4,7 @@ $("head").load("/partials/_head.html");
 $("header").load("/partials/_nav.html");
 $("#testimonials").load("/components/testimonials.html");
 $("#contactForm").load("/components/contactForm.html");
+$("#mymodal").load("/components/modal.html");
 $("footer").load("/partials/_footer.html", ()=>{
 	$("#include").prepend(`<script src="/assets/js/bootstrap.min.js"></script>`);
 	$("#include").prepend(`<script src="/assets/js/popper.min.js"></script>`);
