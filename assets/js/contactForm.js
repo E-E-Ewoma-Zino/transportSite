a5f0ac82a68f206d0902da6d6e4b0e070d664cba
E-E-Ewoma-Zino <eewoma75@gmail.com>
2023-01-04 10:30:22 +0100

    Upgraded contactFrom

--- assets/js/contactForm.js
+++ assets/js/contactForm.js
@@ -1,11 +1,15 @@
 // this script controlls the contact form
 
 const form = document.querySelector("#frudMail");
+const btn = document.querySelector("#mailBtn");
 
 console.log("hefre", form);
 
 form.addEventListener("submit", e => {
 	e.preventDefault();
+
+	btn.innerText = "Sending...";
+	btn.disabled = true;
 
 	const data = {
 		name: form[0].value,
@@ -18,9 +22,17 @@ form.addEventListener("submit", e => {
 
 	axios.post("https://onefinanceblog.com/mail/api", data).then((res) => {
 		console.log("response:::", res);
+
 		$('#centered').modal('show');
+
+		btn.innerText = "Send Mail";
+		btn.disabled = false;
 	}).catch((err) => {
 		console.error("Error:::", err);
+
 		$('#ncentered').modal('show');
+
+		btn.innerText = "Try Again?";
+		btn.disabled = false;
 	});
 });
